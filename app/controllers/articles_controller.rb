9cf0a1a0e1f0908bce78ccef63677849ba30a302
vivekmannotra <vivekmannotra@rocketmail.com>
2015-03-22 14:13:50 +0530

    updating.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-def index
+http_basic_authenticate_with name: "vivek", password: "vivek", except: [:index, :show]
+ 
+  def index
     @articles = Article.all
   end
  
